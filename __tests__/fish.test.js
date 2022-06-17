44fe0e926ef9f81bb55986357c59e24467da00ed
Anddy <alonzoa2015@gmail.com>
2022-06-17 15:24:24 -0700

    fish get all is done and working

---
+++ __tests__/fish.test.js
@@ -0,0 +1,22 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('gets all fish', async () => {
+    const res = await request(app).get('/fishes');
+    const expected = [
+      { 'color': 'Blue', 'id': 1, 'name': 'Nemo', 'weight': 10 }, 
+      { 'color': 'Green', 'id': 2, 'name': 'Dory', 'weight': 20 }, 
+      { 'color': 'Orange', 'id': 3, 'name': 'Marlin', 'weight': 30 }
+    ];
+    expect(res.body).toEqual(expected);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
