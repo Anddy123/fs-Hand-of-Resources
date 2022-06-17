92a8610974a178a021c0fc728fa64b2c7f33596e
Anddy <alonzoa2015@gmail.com>
2022-06-17 15:43:05 -0700

    cat get all is done and working

---
+++ __tests__/cat.test.js
@@ -0,0 +1,24 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('cat routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('gets all cats', async () => {
+    const res = await request(app).get('/cats');
+    const expected = [
+      { 'color': 'Orange', 'id': 1, 'name': 'Garfield', 'type': 'Tiger' },
+      { 'color': 'Black', 'id': 2, 'name': 'Felix', 'type': 'Cat' },
+      { 'color': 'White', 'id': 3, 'name': 'Tom', 'type': 'Cat' }
+    ];
+    expect(res.body).toEqual(expected);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
